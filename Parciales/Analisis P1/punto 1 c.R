72c3b4eb6459093be2bd2af184dcf66c457c61a5
RenixExtreme <ivanrene10@gmail.com>
2021-03-04 09:03:49 -0500

    Update punto 1 c.R

--- Parciales/Analisis P1/punto 1 c.R
+++ Parciales/Analisis P1/punto 1 c.R
@@ -8,7 +8,9 @@ y<-c(0)
 x<-c(0)
 n<-c(0)
 errorarriba<-c(0)
+eaba<-0
 errorabajo<-c(0)
+eajo<-0
 error<-0.1
 E<-0
 E2<-0
@@ -17,15 +19,13 @@ while(i<N+1){
   M<-R*R
   S<-S+M
   E<-0
-  E2<-0
-  E<-(R-error)+(R-error)
-  E2<-(E)*(E)
-  errorabajo[i]<-c(E2)
+  E<-(R-error)*(R-error)
+  errorabajo[i]<-c(E)
+  eajo<-errorabajo[i]+eajo
   E<-0
-  E2<-0
-  E<-(R+error)+(R+error)
-  E2<-(E)*(E)
-  errorarriba[i]<-c(E2)
+  E<-(R+error)*(R+error)
+  errorarriba[i]<-c(E)
+  eaba<-errorarriba[i]+eaba
   y[i]<-c(S)
   x[i]<-c(M)
   n[i]<-c(i)
